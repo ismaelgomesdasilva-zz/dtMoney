54e999b08fb0f761121fc46ff1c040d7a3b6ef7d
Ismael <ismaelgomesdasilva003@gmail.com>
2022-08-01 15:59:58 -0300

    add miragejs and configuration axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { createServer } from "miragejs";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createServer({
+	routes() {
+		this.namespace = "api";
 
+		this.get("/transactions", () => {
+			return [
+				{
+					id: 1,
+					title: "transactions 1",
+					amount: 400,
+					type: "deposit",
+					category: "Food",
+					createAt: new Date(),
+				},
+			];
+		});
+	},
+});
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
