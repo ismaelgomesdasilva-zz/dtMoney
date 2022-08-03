d8c7ec2943319b91e77e57d5605e47fe0e8361cd
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-08-03 11:37:17 -0300

    testing miragejs in producion

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "http://ignite-02-qqfnkehw5-ismaelgomesdasilva.vercel.app/api",
+	baseURL: "http://ignite-02-pxltl5mlt-ismaelgomesdasilva.vercel.app/api",
 });
